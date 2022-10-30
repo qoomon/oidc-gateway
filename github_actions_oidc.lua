5014360908706682983d6fc355dc6ffabedee205
Bengt Brodersen <bengt.brodersen@gmail.com>
2022-10-30 15:01:00 +0100

    refactor

--- github_actions_oidc.lua
+++ github_actions_oidc.lua
@@ -1,24 +1,33 @@
 local openidc = require("resty.openidc")
+
 local token, err = openidc.bearer_jwt_verify({
+    auth_accept_token_as_header_name = "Authorization",
     discovery = "https://token.actions.githubusercontent.com/.well-known/openid-configuration",
     token_signing_alg_values_expected = { "RS256" },
-    auth_accept_token_as_header_name = "Authorization",
     -- iat_slack = math.huge, -- for DEVELOPMENT purpose only
 })
 
 if err or not token then
     ngx.status = ngx.HTTP_UNAUTHORIZED
     ngx.say(err and err or "no access_token provided")
-    ngx.exit(ngx.status)
+    return ngx.exit(ngx.status)
 end
 
+-- Set nginx variables --
+ngx.var.remote_identity = token.sub
+
+-- Check access conditions --
+-- TODO Adjust following conditions to your environment
 local permitted = true
--- permitted = permitted and token.aud:match("^qoomon.me$") ~= nil
-permitted = permitted and token.sub:match("^repo:qoomon/*")
--- permitted = permitted and token.repository_owner:match("^qoomon$")
+-- permitted = permitted and token.aud == "example.com"
+-- permitted = permitted and token.sub:match("^repo:example/*")
+permitted = permitted and token.repository_owner == "example"
 
 if not permitted then
     ngx.status = ngx.HTTP_FORBIDDEN
     ngx.say("Forbidden")
-    ngx.exit(ngx.status)
+    return ngx.exit(ngx.status)
 end
+
+-- Adjust headers ---
+ngx.req.clear_header("Authorization")
